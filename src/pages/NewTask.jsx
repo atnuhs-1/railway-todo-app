1edd6e516f1cd29359877b4368bb42474fe96425
atnuhs <syunnta0033@gmail.com>
2024-08-08 15:31:23 +0900

    upgrade: Library Version Upgrade

--- src/pages/NewTask.jsx
+++ src/pages/NewTask.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { url } from "../const";
 import { Header } from "../components/Header";
 import "./newTask.css"
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const NewTask = () => {
   const [selectListId, setSelectListId] = useState();
@@ -13,7 +13,7 @@ export const NewTask = () => {
   const [detail, setDetail] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [cookies] = useCookies();
-  const history = useHistory();
+  const navigate = useNavigate();
   const handleTitleChange = (e) => setTitle(e.target.value);
   const handleDetailChange = (e) => setDetail(e.target.value);
   const handleSelectList = (id) => setSelectListId(id);
@@ -22,6 +22,7 @@ export const NewTask = () => {
       title: title,
       detail: detail,
       done: false,
+      limit: "2024-12-12T23:59:59Z"
     };
 
     axios.post(`${url}/lists/${selectListId}/tasks`, data, {
@@ -30,7 +31,7 @@ export const NewTask = () => {
         }
     })
     .then(() => {
-      history.push("/");
+      navigate("/");
     })
     .catch((err) => {
       setErrorMessage(`タスクの作成に失敗しました。${err}`);
@@ -46,6 +47,7 @@ export const NewTask = () => {
     .then((res) => {
       setLists(res.data)
       setSelectListId(res.data[0]?.id)
+      console.log(res.data[0]?.id)
     })
     .catch((err) => {
       setErrorMessage(`リストの取得に失敗しました。${err}`);
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,14 +2,14 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useCookies } from "react-cookie";
 import { useSelector, useDispatch } from "react-redux";
-import { useHistory, Redirect } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
 import { signIn } from "../authSlice";
 import { Header } from "../components/Header";
 import { url } from "../const";
 import "./signUp.css";
 
 export const SignUp = () => {
-  const history = useHistory();
+  const navigate = useNavigate();
   const auth = useSelector((state) => state.auth.isSignIn);
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
@@ -20,25 +20,31 @@ export const SignUp = () => {
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handleNameChange = (e) => setName(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  console.log(process.env.REACT_APP_API_URL)
+
   const onSignUp = () => {
     const data = {
       email: email,
       name: name,
       password: password
     };
+    
 
     axios.post(`${url}/users`, data)
       .then((res) => {
         const token = res.data.token;
+        
+        console.log(token);
         dispatch(signIn());
         setCookie("token", token);
-        history.push("/");
+        navigate("/");
       })
       .catch((err) => {
         setErrorMessge(`サインアップに失敗しました。 ${err}`);
       })
 
-      if(auth) return <Redirect to="/" />
+      if(auth) return <Navigate to="/" />
   }
   return (
     <div>
